41f2d150cb7dfac0603e018f55ce493501aeac7f
oersted <ezeiza.j@gmail.com>
2017-03-23 18:38:24 +0000

    Cover the case where there's no date present

--- cordis_scraper.py
+++ cordis_scraper.py
@@ -27,7 +27,8 @@ def extract_entry(doc, name):
         raise
 
 def extract_date(text):
-    return re.match(r'\d\d\d\d-\d\d-\d\d', text).group(0)
+    match = re.match(r'\d\d\d\d-\d\d-\d\d', text)
+    return match.group(0) if match else None
 
 def clean_currency(text):
     return text.replace('EUR', '').replace(' ', '')
@@ -37,7 +38,7 @@ def clean_abstract(text):
 
 def get_project(rcn):
     project_page = requests.get(PROJECT_URL.format(rcn)).text
-    parsed_page = BeautifulSoup(project_page, 'html5lib')
+    parsed_page = BeautifulSoup(project_page)
 
     # Go to https://github.com/pieterheringa/cordis-scraper/blob/master/down.py
     # to see how to extract more attributes
@@ -46,8 +47,12 @@ def get_project(rcn):
 
     dates_section = parsed_page.find(attrs={'class': 'projdates'})
     if dates_section:
-        record['start_date'] = extract_date(extract_entry(dates_section, 'From'))
-        record['end_date'] = extract_date(extract_entry(dates_section, 'To'))
+	start_date = extract_date(extract_entry(dates_section, 'From'))
+	if start_date:
+        	record['start_date'] = start_date
+	end_date = extract_date(extract_entry(dates_section, 'To'))
+	if end_date:
+        	record['end_date'] = end_date
 
     details_section = parsed_page.find(attrs={'class': 'projdet'})
     record['cost'] = clean_currency(extract_entry(details_section, 'Total cost:'))
